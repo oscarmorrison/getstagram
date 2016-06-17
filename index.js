969f9524db4862a0f81d8573d97fa42b215ba45b
Oscar Morrison <omorrison@wanderu.com>
2016-06-17 18:24:57 -0400

    Added express complete instagram

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
 /*jshint esversion: 6 */
 var request = require('request-promise');
 var cheerio = require('cheerio');
+var _ = require('lodash');
 var baseURL = 'https://www.instagram.com/explore/tags/';
-var tag = 'TOSTiceland';
+// var tag = 'TOSTiceland';
 
-request(baseURL+tag)
+var express = require('express');
+
+var app = express();
+
+app.get('/tag/:tag', function(req, res){
+	var tag = req.params.tag;
+	console.log(tag);
+	getTag(tag, res);
+});
+
+app.get('/', function (req, res){
+    res.send({  response: 'Pinging from Getstagram API' });
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function (){
+	instructions = 	[	'Getstagram @ 0.0.0.0:'+port,
+						'/            ping',
+						'/tag/:tag    tag' 
+					];
+	console.log(instructions);
+});
+
+function getTag(tag, res) {
+	request(baseURL+tag)
 	.then(htmlString => {
 		$ = cheerio.load(htmlString);
 		var scripts = $('script[type="text/javascript"]');
 		var data;
 		for(var i in scripts){
-			if(scripts[i].type === 'script'){
-					if(scripts[i].children[0]){
-						if(scripts[i].children[0].data){
-							if(/_sharedData/.test(scripts[i].children[0].data)){
-								data = scripts[i].children[0].data;
-								data = data.replace('window._sharedData = ', '');
-								data = data.substr(0, data.length-1);
-								// data = JSON.parse(data);
-							}
-						}
-					}	
+			if(	scripts[i].type === 'script' && 
+				scripts[i].children[0] && 
+				scripts[i].children[0].data && 
+				/_sharedData/.test(scripts[i].children[0].data) 
+				){
+				data = scripts[i].children[0].data.replace('window._sharedData = ', '');
+				data = data.substr(0, data.length-1);
+				data = cleanData(JSON.parse(data));
+				res.send({posts: data});
 			}
 		}
-		console.log(data);
-		// scripts.forEach(function(item){ 
-		// 	console.log(item.text);
-		// 	if(/window._sharedData/.test(scripts[i].text)){
-		// 		data = scripts[i].text;
-		// 	}
-		// });
-		// console.log(data);
 	});
+
+	function cleanData(data){
+		data = _.pick(data, 'entry_data').entry_data.TagPage[0].tag.media.nodes;
+		data = data.map( entry => {
+			return _.pick(entry, ['date', 'caption', 'dimensions', 'caption', 'likes', 'thumbnail_src', 'is_video', 'display_src']);
+		});
+		return data;
+	}
+}
