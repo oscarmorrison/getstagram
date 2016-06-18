ac28f8620631b1f1c554f228f3eeaaad55839385
Oscar Morrison <omorrison@wanderu.com>
2016-06-17 23:05:29 -0400

    add CORS

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ var express = require('express');
 
 var app = express();
 
+app.use(function (req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
+
 app.get('/tag/:tag', function(req, res){
 	var tag = req.params.tag;
 	console.log(tag);
